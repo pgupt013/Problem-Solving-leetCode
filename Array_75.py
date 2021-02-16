7ed1738f5234737b36217cb0a6a07ea7ae49bfff
pgupt013 <64878072+pgupt013@users.noreply.github.com>
2021-02-16 12:03:42 -0500

    Update Array_75.py

--- Array_75.py
+++ Array_75.py
@@ -1,4 +1,8 @@
+## 75. Sort Colors
+## Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white, and blue.
+## We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
 arr=[2,0,2,1,1,0]
+##arr=[2,0,1]
 low=0
 mid=0
 high=len(arr)-1
