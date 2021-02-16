9ce9363e2ddf484aaab874cb14635f79e3ad2c65
pgupt013 <64878072+pgupt013@users.noreply.github.com>
2021-02-16 11:38:37 -0500

    Update Array_905.py

--- Array_905.py
+++ Array_905.py
@@ -1,3 +1,5 @@
+## 905. Sort Array By Parity
+## Given an array A of non-negative integers, return an array consisting of all the even elements of A, followed by all the odd elements of A.
 l=[3,1,2,4]
 i=0
 j=len(l)-1
